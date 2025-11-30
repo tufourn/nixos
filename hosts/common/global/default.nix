4b237657656488bbbce3986fbdc4ea506a93530e
Tuan Nguyen <noreply@tufourn.com>
2025-11-30 15:27:48 +0700

    more modularization

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -6,4 +6,17 @@
     ./fish.nix
     ./keyboard.nix
   ];
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  programs = {
+    git.enable = true;
+    vim.enable = true;
+  };
+
+  environment.variables.EDITOR = "vim";
 }
--- hosts/nix280/default.nix
+++ hosts/nix280/default.nix
@@ -38,18 +38,11 @@
     packages = with pkgs; [];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    git
-    vim
     wl-clipboard
   ];
-
-  environment.variables.EDITOR = "vim";
 
   programs.hyprland.enable = true;
 
